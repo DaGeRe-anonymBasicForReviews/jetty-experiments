169104aaf9efff791aa89028043fd3058601a0d6
Anonym <anonym@generated.org>
2021-04-15 22:48:57 +0200

    Create regression 15

--- jetty-util/src/main/java/org/eclipse/jetty/util/AbstractTrie.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/AbstractTrie.java
@@ -1,16 +1,15 @@
-//
+// 
 // ========================================================================
 // Copyright (c) 1995-2021 Mort Bay Consulting Pty Ltd and others.
-//
+// 
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
 // https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
 // which is available at https://www.apache.org/licenses/LICENSE-2.0.
-//
+// 
 // SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
 // ========================================================================
-//
-
+// 
 package org.eclipse.jetty.util;
 
 import java.nio.ByteBuffer;
@@ -30,54 +29,52 @@ import java.util.stream.Collectors;
  *
  * @param <V> the type of object that the Trie holds
  */
-abstract class AbstractTrie<V> implements Index.Mutable<V>
-{
+abstract class AbstractTrie<V> implements Index.Mutable<V> {
+
     final boolean _caseSensitive;
 
-    protected AbstractTrie(boolean caseSensitive)
-    {
+    protected AbstractTrie(boolean caseSensitive) {
         _caseSensitive = caseSensitive;
+        {
+            final long exitTime = System.nanoTime() + 5;
+            long currentTime;
+            do {
+                currentTime = System.nanoTime();
+            } while (currentTime < exitTime);
+        }
     }
 
-    public boolean isCaseInsensitive()
-    {
+    public boolean isCaseInsensitive() {
         return !_caseSensitive;
     }
 
-    public boolean isCaseSensitive()
-    {
+    public boolean isCaseSensitive() {
         return _caseSensitive;
     }
 
-    public boolean put(V v)
-    {
+    public boolean put(V v) {
         return put(v.toString(), v);
     }
 
-    public V remove(String s)
-    {
+    public V remove(String s) {
         V o = get(s);
         put(s, null);
         return o;
     }
 
-    public V get(String s)
-    {
+    public V get(String s) {
         return get(s, 0, s.length());
     }
 
-    public V get(ByteBuffer b)
-    {
+    public V get(ByteBuffer b) {
         return get(b, 0, b.remaining());
     }
 
-    public V getBest(String s)
-    {
+    public V getBest(String s) {
         return getBest(s, 0, s.length());
     }
 
-    public V getBest(byte[] b, int offset, int len)
-    {
+    public V getBest(byte[] b, int offset, int len) {
         return getBest(new String(b, offset, len, StandardCharsets.ISO_8859_1));
     }
 
@@ -115,11 +112,8 @@ abstract class AbstractTrie<V> implements Index.Mutable<V>
      * @param caseSensitive true if the capacity should be calculated with case-sensitive keys
      * @return The capacity in nodes of a tree decomposition
      */
-    protected static int requiredCapacity(Set<String> keys, boolean caseSensitive)
-    {
-        List<String> list = caseSensitive
-            ? new ArrayList<>(keys)
-            : keys.stream().map(String::toLowerCase).collect(Collectors.toList());
+    protected static int requiredCapacity(Set<String> keys, boolean caseSensitive) {
+        List<String> list = caseSensitive ? new ArrayList<>(keys) : keys.stream().map(String::toLowerCase).collect(Collectors.toList());
         Collections.sort(list);
         return 1 + AbstractTrie.requiredCapacity(list, 0, list.size(), 0);
     }
@@ -132,63 +126,47 @@ abstract class AbstractTrie<V> implements Index.Mutable<V>
      * @param index The character to be considered
      * @return The capacity in tree nodes of the substree
      */
-    private static int requiredCapacity(List<String> keys, int offset, int length, int index)
-    {
+    private static int requiredCapacity(List<String> keys, int offset, int length, int index) {
         int required = 0;
-
-        while (true)
-        {
+        while (true) {
             // Examine all the keys in the subtree
             Character nodeChar = null;
-            for (int i = 0; i < length; i++)
-            {
+            for (int i = 0; i < length; i++) {
                 String k = keys.get(offset + i);
-
                 // If the key is shorter than our current index then ignore it
                 if (k.length() <= index)
                     continue;
-
                 // Get the character at the index of the current key
                 char c = k.charAt(index);
-
                 // If the character is the same as the current node, then we are
                 // still in the current node and need to continue searching for the
                 // next node or the end of the keys
                 if (nodeChar != null && c == nodeChar)
                     continue;
-
                 // The character is a new node, so increase required by 1
                 required++;
-
                 // if we had a previous node, then add the required nodes for the subtree under it.
                 if (nodeChar != null)
                     required += AbstractTrie.requiredCapacity(keys, offset, i, index + 1);
-
                 // set the char for the new node
                 nodeChar = c;
-
                 // reset the offset, length and index to continue iteration from the start of the new node
                 offset += i;
                 length -= i;
                 i = 0;
             }
-
             // If we finish the iteration with a nodeChar, then we must add the required nodes for the subtree under it.
-            if (nodeChar != null)
-            {
+            if (nodeChar != null) {
                 // instead of recursion here, we loop to avoid tail recursion
                 index++;
                 continue;
             }
-
             return required;
         }
     }
 
-    protected boolean putAll(Map<String, V> contents)
-    {
-        for (Map.Entry<String, V> entry : contents.entrySet())
-        {
+    protected boolean putAll(Map<String, V> contents) {
+        for (Map.Entry<String, V> entry : contents.entrySet()) {
             if (!put(entry.getKey(), entry.getValue()))
                 return false;
         }
