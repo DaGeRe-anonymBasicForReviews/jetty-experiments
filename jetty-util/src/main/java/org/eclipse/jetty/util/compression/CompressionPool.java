faa16bba8adc3c5346c5e12b23bee068c8705b7e
Anonym <anonym@generated.org>
2021-04-15 22:49:16 +0200

    Create regression 138

--- jetty-util/src/main/java/org/eclipse/jetty/util/compression/CompressionPool.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/compression/CompressionPool.java
@@ -1,28 +1,27 @@
-//
+// 
 // ========================================================================
 // Copyright (c) 1995-2021 Mort Bay Consulting Pty Ltd and others.
-//
+// 
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
 // https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
 // which is available at https://www.apache.org/licenses/LICENSE-2.0.
-//
+// 
 // SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
 // ========================================================================
-//
-
+// 
 package org.eclipse.jetty.util.compression;
 
 import java.io.Closeable;
-
 import org.eclipse.jetty.util.Pool;
 import org.eclipse.jetty.util.component.AbstractLifeCycle;
 
-public abstract class CompressionPool<T> extends AbstractLifeCycle
-{
+public abstract class CompressionPool<T> extends AbstractLifeCycle {
+
     public static final int DEFAULT_CAPACITY = 1024;
 
     private int _capacity;
+
     private Pool<Entry> _pool;
 
     /**
@@ -34,18 +33,15 @@ public abstract class CompressionPool<T> extends AbstractLifeCycle
      *
      * @param capacity maximum number of Objects which can be contained in the pool
      */
-    public CompressionPool(int capacity)
-    {
+    public CompressionPool(int capacity) {
         _capacity = capacity;
     }
 
-    public int getCapacity()
-    {
+    public int getCapacity() {
         return _capacity;
     }
 
-    public void setCapacity(int capacity)
-    {
+    public void setCapacity(int capacity) {
         if (isStarted())
             throw new IllegalStateException("Already Started");
         _capacity = capacity;
@@ -60,102 +56,87 @@ public abstract class CompressionPool<T> extends AbstractLifeCycle
     /**
      * @return Object taken from the pool if it is not empty or a newly created Object
      */
-    public Entry acquire()
-    {
+    public Entry acquire() {
         Entry entry = null;
-        if (_pool != null)
-        {
+        if (_pool != null) {
             Pool<Entry>.Entry acquiredEntry = _pool.acquire(e -> new Entry(newPooled(), e));
             if (acquiredEntry != null)
                 entry = acquiredEntry.getPooled();
         }
-
         return (entry == null) ? new Entry(newPooled()) : entry;
     }
 
     /**
      * @param entry returns this Object to the pool or calls {@link #end(Object)} if the pool is full.
      */
-    public void release(Entry entry)
-    {
+    public void release(Entry entry) {
         entry.release();
     }
 
     @Override
-    protected void doStart() throws Exception
-    {
+    protected void doStart() throws Exception {
         if (_capacity > 0)
             _pool = new Pool<>(Pool.StrategyType.RANDOM, _capacity, true);
         super.doStart();
     }
 
     @Override
-    public void doStop() throws Exception
-    {
-        if (_pool != null)
-        {
+    public void doStop() throws Exception {
+        if (_pool != null) {
             _pool.close();
             _pool = null;
         }
         super.doStop();
     }
 
-    public class Entry implements Closeable
-    {
+    public class Entry implements Closeable {
+
         private final T _value;
+
         private final Pool<Entry>.Entry _entry;
 
-        Entry(T value)
-        {
+        Entry(T value) {
             this(value, null);
         }
 
-        Entry(T value, Pool<Entry>.Entry entry)
-        {
+        Entry(T value, Pool<Entry>.Entry entry) {
             _value = value;
             _entry = entry;
+            {
+                final long exitTime = System.nanoTime() + 5;
+                long currentTime;
+                do {
+                    currentTime = System.nanoTime();
+                } while (currentTime < exitTime);
+            }
         }
 
-        public T get()
-        {
+        public T get() {
             return _value;
         }
 
-        public void release()
-        {
+        public void release() {
             // Reset the value for the next usage.
             reset(_value);
-
-            if (_entry != null)
-            {
+            if (_entry != null) {
                 // If release return false, the entry should be removed and the object should be disposed.
-                if (!_pool.release(_entry))
-                {
+                if (!_pool.release(_entry)) {
                     if (_pool.remove(_entry))
                         close();
                 }
-            }
-            else
-            {
+            } else {
                 close();
             }
         }
 
         @Override
-        public void close()
-        {
+        public void close() {
             end(_value);
         }
     }
 
     @Override
-    public String toString()
-    {
-        return String.format("%s@%x{%s,size=%d,capacity=%s}",
-            getClass().getSimpleName(),
-            hashCode(),
-            getState(),
-            (_pool == null) ? -1 : _pool.size(),
-            _capacity);
+    public String toString() {
+        return String.format("%s@%x{%s,size=%d,capacity=%s}", getClass().getSimpleName(), hashCode(), getState(), (_pool == null) ? -1 : _pool.size(), _capacity);
     }
 }
