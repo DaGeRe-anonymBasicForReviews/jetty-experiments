e852a6bbe840498c59700e925783caf1c4f22be5
Anonym <anonym@generated.org>
2021-04-15 22:51:28 +0200

    Create regression 988

--- jetty-util/src/main/java/org/eclipse/jetty/util/thread/strategy/ProduceExecuteConsume.java
+++ jetty-util/src/main/java/org/eclipse/jetty/util/thread/strategy/ProduceExecuteConsume.java
@@ -1,20 +1,18 @@
-//
+// 
 // ========================================================================
 // Copyright (c) 1995-2021 Mort Bay Consulting Pty Ltd and others.
-//
+// 
 // This program and the accompanying materials are made available under the
 // terms of the Eclipse Public License v. 2.0 which is available at
 // https://www.eclipse.org/legal/epl-2.0, or the Apache License, Version 2.0
 // which is available at https://www.apache.org/licenses/LICENSE-2.0.
-//
+// 
 // SPDX-License-Identifier: EPL-2.0 OR Apache-2.0
 // ========================================================================
-//
-
+// 
 package org.eclipse.jetty.util.thread.strategy;
 
 import java.util.concurrent.Executor;
-
 import org.eclipse.jetty.util.thread.AutoLock;
 import org.eclipse.jetty.util.thread.ExecutionStrategy;
 import org.eclipse.jetty.util.thread.Invocable;
@@ -26,32 +24,37 @@ import org.slf4j.LoggerFactory;
  * <p>A strategy where the caller thread iterates over task production, submitting each
  * task to an {@link Executor} for execution.</p>
  */
-public class ProduceExecuteConsume implements ExecutionStrategy
-{
+public class ProduceExecuteConsume implements ExecutionStrategy {
+
     private static final Logger LOG = LoggerFactory.getLogger(ProduceExecuteConsume.class);
 
     private final AutoLock _lock = new AutoLock();
+
     private final Producer _producer;
+
     private final Executor _executor;
+
     private State _state = State.IDLE;
 
-    public ProduceExecuteConsume(Producer producer, Executor executor)
-    {
+    public ProduceExecuteConsume(Producer producer, Executor executor) {
         _producer = producer;
         _executor = executor;
     }
 
     @Override
-    public void produce()
-    {
-        try (AutoLock lock = _lock.lock())
+    public void produce() {
         {
-            switch (_state)
-            {
+            final long exitTime = System.nanoTime() + 5;
+            long currentTime;
+            do {
+                currentTime = System.nanoTime();
+            } while (currentTime < exitTime);
+        }
+        try (AutoLock lock = _lock.lock()) {
+            switch(_state) {
                 case IDLE:
                     _state = State.PRODUCE;
                     break;
-
                 case PRODUCE:
                 case EXECUTE:
                     _state = State.EXECUTE;
@@ -60,21 +63,15 @@ public class ProduceExecuteConsume implements ExecutionStrategy
                     throw new IllegalStateException(_state.toString());
             }
         }
-
         // Produce until we no task is found.
-        while (true)
-        {
+        while (true) {
             // Produce a task.
             Runnable task = _producer.produce();
             if (LOG.isDebugEnabled())
                 LOG.debug("{} produced {}", _producer, task);
-
-            if (task == null)
-            {
-                try (AutoLock lock = _lock.lock())
-                {
-                    switch (_state)
-                    {
+            if (task == null) {
+                try (AutoLock lock = _lock.lock()) {
+                    switch(_state) {
                         case IDLE:
                             throw new IllegalStateException();
                         case PRODUCE:
@@ -88,7 +85,6 @@ public class ProduceExecuteConsume implements ExecutionStrategy
                     }
                 }
             }
-
             // Execute the task.
             if (Invocable.getInvocationType(task) == InvocationType.NON_BLOCKING)
                 task.run();
@@ -98,13 +94,12 @@ public class ProduceExecuteConsume implements ExecutionStrategy
     }
 
     @Override
-    public void dispatch()
-    {
+    public void dispatch() {
         _executor.execute(this::produce);
     }
 
-    private enum State
-    {
+    private enum State {
+
         IDLE, PRODUCE, EXECUTE
     }
 }
